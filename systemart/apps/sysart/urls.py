ce8fb4daa1a80c13658f399b9acfd66eb215bff6
nickol <145790372+equqe@users.noreply.github.com>
2024-06-01 14:22:28 +0300

    fin v1

--- systemart/apps/sysart/urls.py
+++ systemart/apps/sysart/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from apps.main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,4 +41,4 @@ urlpatterns = [
     path('casesets/<int:pk>/edit/', views.edit_caseset.as_view(), name='edit_caseset'),
 
     path('', views.index_view),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
